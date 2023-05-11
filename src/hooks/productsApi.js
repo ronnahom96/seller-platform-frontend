50211dbfc718c375b071064b31c2b9abe153a85c
ronnahom96 <ronnahom96@gmail.com>
2023-05-11 11:26:30 +0300

    connect to the server with react query

---
+++ src/hooks/productsApi.js
@@ -0,0 +1,39 @@
+import { useQuery, useMutation } from 'react-query';
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3000/products';
+
+export function useCreateProduct() {
+    return useMutation(async (product) => {
+        const response = await axios.post(`${BASE_URL}`, product);
+        return response.data;
+    });
+}
+
+export function useReadProduct(asin, locale) {
+    return useQuery(['productById', asin, locale], async () => {
+        const response = await axios.get(`${BASE_URL}/${asin}/${locale}`);
+        return response.data;
+    });
+}
+
+export function useUpdateProduct() {
+    return useMutation(async (asin, locale, productToUpdate) => {
+        const response = await axios.put(`${BASE_URL}/${asin}/${locale}`, productToUpdate);
+        return response.data;
+    });
+}
+
+export function useDeleteBatchProducts() {
+    return useMutation(async (productIds) => {
+        const response = await axios.post(`${BASE_URL}/delete-batch`, { productIds });
+        return response.data;
+    });
+}
+
+export function useReadProducts(sellerName, availability = true) {
+    return useQuery(['productBySellerName', sellerName], async () => {
+        const response = await axios.get(`${BASE_URL}?sellerName=${sellerName}&availability=${availability}`);
+        return response.data;
+    });
+}
